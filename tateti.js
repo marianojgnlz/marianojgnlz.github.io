3f629b94e154c5a3d077a43034648723cf2f3edd
Mariano Gonzalez <mariano.gonzalez@urbetrack.com>
2023-09-13 20:57:12 -0300

    tateti

---
+++ tateti.js
@@ -0,0 +1,57 @@
+const ip = "192.168.1.191";
+const port = "1000";
+
+const myUsername = prompt("Please enter your name") || "Anonymous";
+const socket = new WebSocket(
+  `ws://${ip}:${port}/tateti?username=${myUsername}`
+);
+
+socket.onmessage = (m) => {
+  const data = JSON.parse(m.data);
+  switch (data.event) {
+    case "start":
+        localStorage.setItem(data.player1.userName, data.player1.playWith)
+        localStorage.setItem(data.player2.userName, data.player2.playWith)
+      break;
+    case "set-user":
+      console.log(data)
+      document.querySelector(".player").innerHTML = data.username;
+    break;
+    case "send-message":
+      // display new chat message
+      addMessage(data.username, data.message);
+      break;
+    case "click":
+      const $elm = document.querySelector(`#${data.element}`);
+      console.log($elm);
+      $elm.innerHTML = localStorage.getItem(data.username);
+      break;
+  }
+};
+
+function addMessage(username, message) {
+  // displays new message
+  document.getElementById(
+    "conversation"
+  ).innerHTML += `<b> ${username} </b>: ${message} <br/>`;
+}
+
+// on page load
+window.onload = () => {
+    const $elements = document.querySelectorAll(".cell");
+  // when the client hits the ENTER key
+  $elements.forEach($e => {
+    $e.addEventListener("click", HandleClick)
+  });
+};
+
+
+function HandleClick(e) {
+  socket.send(
+    JSON.stringify({
+      event: "click",
+      element: e.target.id
+    })
+  )
+}
+
